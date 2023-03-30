b75932b78c4b6eff271ae88d611c090e14630aa6
P. Alexander Burnham <pburnham@uvm.edu>
2023-03-30 10:30:28 -0400

    pandas added to dependancies

--- R/spacetime_wrapper.R
+++ R/spacetime_wrapper.R
@@ -51,6 +51,7 @@ conda_install("r-reticulate", "xarray")
 conda_install("r-reticulate", "psutil")
 conda_install("r-reticulate", "plotly_express")
 conda_install("r-reticulate", "netCDF4")
+conda_install("r-reticulate", "pandas")
 
 # activate environment
 use_condaenv(condaenv = "r-reticulate", conda = "auto", required = TRUE)
