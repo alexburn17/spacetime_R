b567d4dcc35aadde57e9b227fd0581fbe7e706f0
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-10 10:53:49 -0400

    update

--- .Rproj.user/41985A3C/sources/session-358dcaac/86CC151E-contents
+++ .Rproj.user/41985A3C/sources/session-358dcaac/86CC151E-contents
@@ -57,7 +57,7 @@ conda_install("r-reticulate", "netCDF4")
 do_a_thing<- function(data){
   
   
-  t <- spacetime::get_array()
+  t <- get_array()
   
   return(t)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,10 @@ get_array <- NULL
   library(reticulate)
   use_condaenv(condaenv = "r-reticulate", conda = "auto", required = TRUE)
 
-  spacetime_test <- reticulate::import_from_path("spacetime_test", (system.file("python/input",
+  spacetime_test <<- reticulate::source_python(system.file("python/input",
                                                           "spacetime_test.py",
                                                           package = "spacetime",
-                                                          mustWork = TRUE)))
+                                                          mustWork = TRUE))
   get_array <<- spacetime_test$get_array
 
 
