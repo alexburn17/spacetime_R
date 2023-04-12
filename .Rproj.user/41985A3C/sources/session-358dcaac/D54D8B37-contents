1cea2ad8449dfd8356d78f9554ae5d63c5ceba17
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-12 11:51:08 -0400

    installation updated

--- .Rproj.user/41985A3C/sources/session-358dcaac/D54D8B37-contents
+++ .Rproj.user/41985A3C/sources/session-358dcaac/D54D8B37-contents
@@ -8,6 +8,15 @@ dependancies <- c("pandas", "numpy")
 #install_miniconda(path = miniconda_path(), update = TRUE, force = TRUE)
 
 
+user_permission <- utils::askYesNo("Install miniconda? downloads 50MB and takes time")
+
+if (isTRUE(user_permission)) {
+  reticulate::install_miniconda()
+} else {
+  message("You should run `reticulate::install_miniconda()` before using this package")
+}
+
+
 py_install(
   packages = dependancies,
   method = "conda",
--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,17 @@
 get_array <- NULL
+read_data <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  user_permission <- utils::askYesNo("Install miniconda? downloads 50MB and takes time")
-  
-  if (isTRUE(user_permission)) {
-    reticulate::install_miniconda()
-  } else {
-    message("You should run `reticulate::install_miniconda()` before using this package")
-  }
-  
-  
+
   use_condaenv(condaenv = "r-reticulate", conda = "auto", required = TRUE)
   
   the_mod <- reticulate::import_from_path(module = "spacetime_test", system.file("python", "input", package = "spacetime", mustWork = TRUE))
+  readData <- reticulate::import_from_path(module = "readData", system.file("python", "input", package = "spacetime", mustWork = TRUE))
+  
+  
+  
   get_array <<- the_mod$get_array
+  read_data <<- readData$read_data
   
   
 }
