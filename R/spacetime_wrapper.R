f1b233f491294d84997806ac0ab008c924f7fdef
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-12 15:07:27 -0400

    update to wrapper

--- R/spacetime_wrapper.R
+++ R/spacetime_wrapper.R
@@ -16,14 +16,14 @@ library(reticulate)
 
 
 
-install_miniconda <- function(){
+install_python_environment <- function(){
   
-  user_permission <- utils::askYesNo("Install miniconda? downloads 50MB and takes time")
+  user_permission <- utils::askYesNo("Install python environment through miniconda? Miniconda is required for spacetime to run. If you havent already installed it, type yes")
 
   if (isTRUE(user_permission)) {
     reticulate::install_miniconda(path = miniconda_path(), force = TRUE)
   } else {
-    message("You should run `reticulate::install_miniconda()` before using this package")
+    message("You are all set!")
   }
 
 }
@@ -80,123 +80,6 @@ make_cube <- function(data, fileName, organizeFiles, organizeBands, varNames = "
 }
 
 
-
-
-
-
-
-#############################################
-######### spacetime method wrappers #########
-#############################################
-
-# get lat wrapper
-get_lat <- function(obj){
-  
-  x = obj$get_lat()
-  return(x)
-}
-
-
-# get lon wrapper
-get_lon <- function(obj){
-  
-  x = obj$get_lon()
-  return(x)
-}
-
-# get UL corner wrapper
-get_UL_corner <- function(obj){
-  
-  x = obj$get_UL_corner()
-
-  return(x)
-}
-
-
-# get EPSG code wrapper
-get_epsg_code <- function(obj){
-  
-  x = obj$get_epsg_code()
-  
-  return(x)
-}
-
-
-
-# get units wrapper
-get_units <- function(obj){
-  
-  x = obj$get_units()
-  
-  return(x)
-}
-
-
-# get band number wrapper
-get_band_number <- function(obj){
-  
-  x = obj$get_band_number()
-  
-  return(x)
-}
-
-
-# get raster dims wrapper
-get_dims <- function(obj){
-  
-  x = unlist(obj$get_dims())
-  x <- split(x, ceiling(seq_along(unlist(x))/2))
-  
-  return(x)
-}
-
-
-
-# get nodata value wrapper
-get_nodata_value <- function(obj){
-  
-  x = obj$get_nodata_value()
-  
-  return(x)
-}
- 
-
-# get data wrapper
-get_data_array <- function(obj){
-  
-  x = obj$get_data_array()
-  
-  return(x)
-}
-
-
-
-# get GDAL data wrapper
-get_GDAL_data <- function(obj){
-  
-  x = obj$get_GDAL_data()
-  
-  return(x)
-}
-
-
-
-# get time wrapper
-get_time <- function(obj){
-  
-  x = obj$get_time()
-  
-  return(x)
-}
-
-
-# get var names wrapper
-get_var_names <- function(obj){
-  
-  x = obj$get_var_names()
-  
-  return(x)
-}
 
 
 
