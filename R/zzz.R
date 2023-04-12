fb984dbe67d2375a78228cdc7b93dbb7d36f318c
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-12 10:34:56 -0400

    added miniconda install

--- R/zzz.R
+++ R/zzz.R
@@ -7,14 +7,4 @@ get_array <- NULL
   the_mod <- reticulate::import_from_path(module = "spacetime_test", system.file("python", "input", package = "spacetime", mustWork = TRUE))
   get_array <<- the_mod$get_array
   
-
-  
-  
-#  user_permission <- utils::askYesNo("Install miniconda? downloads 50MB and takes time")
-#  
-#  if (isTRUE(user_permission)) {
-#    reticulate::install_miniconda(path = miniconda_path(), update = TRUE, force = TRUE)
-#  } else {
-#    message("You should run `reticulate::install_miniconda()` before using this package")
-#  }
 }
