b7ced0ddcb1df4bbff4bcb3bc5546a999fa01415
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-12 12:40:59 -0400

    update to zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,16 @@
 get_array <- NULL
-#read_data <- NULL
+read_data <- NULL
 
 .onLoad <- function(libname, pkgname) {
 
   use_condaenv(condaenv = "r-reticulate", conda = "auto", required = TRUE)
   
   the_mod <- reticulate::import_from_path(module = "spacetime_test", system.file("python", "input", package = "spacetime", mustWork = TRUE))
-  #readData <- reticulate::import_from_path(module = "readData", system.file("python", "input", package = "spacetime", mustWork = TRUE))
+  read_d <- reticulate::import_from_path(module = "readData", system.file("python", "input", package = "spacetime", mustWork = TRUE))
   
   
   get_array <<- the_mod$get_array
-  #read_data <<- readData$read_data
+  read_data <<- read_d$read_data
   
   
 }
