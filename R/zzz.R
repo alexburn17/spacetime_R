cbf192264a52e816d86d03574519fdbd8938c0b3
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-17 14:51:51 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 get_array <- NULL
 read_data <- NULL
 #file_object <- NULL
-#raster_align <- NULL
+raster_align <- NULL
 #raster_trim <- NULL
-#make_cube <- NULL\
+#make_cube <- NULL
 
 .onLoad <- function(libname, pkgname) {
 
@@ -12,14 +12,14 @@ read_data <- NULL
   the_mod <- reticulate::import_from_path(module = "spacetimetest", system.file("python", "input", package = "spacetime", mustWork = TRUE))
   read_d <- reticulate::import_from_path(module = "readData", system.file("python", "input", package = "spacetime", mustWork = TRUE))
   #file_o <- reticulate::import_from_path(module = "fileObject", system.file("python", "objects", package = "spacetime", mustWork = TRUE))
-  #raster_a <- reticulate::import_from_path(module = "rasterAlign", system.file("python", "scale", package = "spacetime", mustWork = TRUE))
+  raster_a <- reticulate::import_from_path(module = "rasterAlign", system.file("python", "scale", package = "spacetime", mustWork = TRUE))
   #raster_t <- reticulate::import_from_path(module = "rasterTrim", system.file("python", "scale", package = "spacetime", mustWork = TRUE))
   #make_c <- reticulate::import_from_path(module = "makeCube", system.file("python", "operations", package = "spacetime", mustWork = TRUE))
   
   get_array <<- the_mod$get_array
   read_data <<- read_d$read_data
   #file_object <<- file_o$file_object
-  #raster_align <<- raster_a$raster_align
+  raster_align <<- raster_a$raster_align
   #raster_trim <<- raster_t$raster_trim
   #make_cube <<- make_c$make_cube
 }
