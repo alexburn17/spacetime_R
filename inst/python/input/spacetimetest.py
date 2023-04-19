c993404bedcb1c36281fa025aac76c04d1430bb0
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-19 13:55:01 -0400

    Update spacetimetest.py

--- inst/python/input/spacetimetest.py
+++ inst/python/input/spacetimetest.py
@@ -3,11 +3,14 @@
 def get_array():
   import numpy as np
   import os
+  
+  import pathlib
+  x = pathlib.Path(__file__).parent.resolve()
 
   
   
   #x = np.array([1, 2, 3, 4])
-  x = os.getcwdb()
+  #x = os.getcwdb()
   
   return(x)
 
